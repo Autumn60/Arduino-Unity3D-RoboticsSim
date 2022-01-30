821ff686393fd87cb98c416195e3490f5847d400
A-Harada <r117159@g.kobe-kosen.ac.jp>
2022-01-30 21:52:33 +0900

    Create AirCylinder(in Unity)

---
+++ Unity/Arduino-Unity3D-RoboticsSim/Assets/Scripts/Modules/Actuators/AirCylinder.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirCylinder : Module
+{
+    AirCylinder()
+    {
+        moduleType = Module.ModuleType.Actuator;
+    }
+
+    enum Axis
+    {
+        X,
+        Y,
+        Z
+    };
+
+    [SerializeField]
+    private ConfigurableJoint joint;
+
+    [SerializeField]
+    private Axis axis;
+
+    [SerializeField]
+    private float distance = 1.0f;
+
+    [SerializeField]
+    private float positionSpring = 50.0f;
+
+    [SerializeField]
+    private float positionDamper = 10.0f;
+
+    [SerializeField]
+    private float maxsimumForce = 100.0f;
+
+    private SoftJointLimit jointLimit;
+    private JointDrive xDrive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint.autoConfigureConnectedAnchor = false;
+
+        jointLimit.bounciness = 0.0f;
+        jointLimit.contactDistance = 0.0f;
+        jointLimit.limit = distance * 0.5f;
+
+        xDrive.positionSpring = positionSpring;
+        xDrive.positionDamper = positionDamper;
+        xDrive.maximumForce = maxsimumForce;
+
+        joint.xDrive = xDrive;
+
+        joint.linearLimit = jointLimit;
+        joint.xMotion = ConfigurableJointMotion.Limited;
+        joint.yMotion = ConfigurableJointMotion.Locked;
+        joint.zMotion = ConfigurableJointMotion.Locked;
+
+        switch (axis)
+        {
+            case Axis.X:
+                joint.axis = new Vector3(1, 0, 0);
+                joint.connectedAnchor = new Vector3(0, distance*0.5f, 0);
+                break;
+            case Axis.Y:
+                joint.axis = new Vector3(0, 1, 0);
+                joint.connectedAnchor = new Vector3(-distance * 0.5f, 0, 0);
+                break;
+            case Axis.Z:
+                joint.axis = new Vector3(0, 0, 1);
+                joint.connectedAnchor = new Vector3(0, 0, -distance * 0.5f);
+                break;
+        }
+        joint.angularXMotion = ConfigurableJointMotion.Locked;
+        joint.angularYMotion = ConfigurableJointMotion.Locked;
+        joint.angularZMotion = ConfigurableJointMotion.Locked;
+
+        Off();
+    }
+
+    void On()
+    {
+        joint.targetPosition = new Vector3(distance * 0.5f, 0, 0);
+    }
+
+    void Off()
+    {
+        joint.targetPosition = new Vector3(-distance * 0.5f, 0, 0);
+    }
+
+    public override void SetValue(string newState)
+    {
+        if (newState == null) return;
+        if (newState == "0")
+            Off();
+        if (newState == "1")
+            On();
+    }
+}
